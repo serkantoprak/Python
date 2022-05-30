f732b8f15bd0f8b40ddc419ce8fb283eeb8389bb
serkantoprak <serkan.toprakk8@gmail.com>
2022-05-30 14:47:48 +0300

    change

--- basamaklı sayılar.py
+++ basamaklı sayılar.py
@@ -1,9 +1,9 @@
 liste=[]
 
-sayi=1000000000000000000000
+sayi=1000
 toplam=0
 
-while sayi < 10000000000000000000000:
+while sayi < 1000:
     for rakam in str(sayi):
         toplam +=int(rakam)
     if toplam ==99:
@@ -12,4 +12,3 @@ while sayi < 10000000000000000000000:
     toplam=0
 print(f"rakamlar toplamı 99 olan, {len(liste)}adet, 22 basamaklı sayı vardır")
 print(liste)
-print("hello world")
