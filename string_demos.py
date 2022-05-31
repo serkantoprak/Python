63622eb46ec8523481e965482f72c1c10292169c
serkantoprak <serkan.toprakk8@gmail.com>
2022-05-31 20:36:03 +0300

    dosyalar

---
+++ string_demos.py
@@ -0,0 +1,16 @@
+from unittest import result
+
+
+website = 'www.serkantoprak.com'
+course = 'Yes of course my friend'
+
+#1 ' Hello world ' karakter dizisinin baş ve sondaki boşlukları silme
+   result = ' Hello World '.strip()
+   
+ 
+#2 'www.serkantoprak.com' içindeki serkantoprak haricindeki tüm karakterleri silme
+
+#3 'CoUrSe' tüm harfleri küçük yapma
+
+
+  print(result)
