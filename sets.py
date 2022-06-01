af362170dae889733e16c72454e8b8436f3d8b85
serkantoprak <serkan.toprakk8@gmail.com>
2022-06-02 02:06:11 +0300

    unit3-son

---
+++ sets.py
@@ -0,0 +1,25 @@
+fruits = {'orange','apple','banana'}
+
+#print(fruits[0]) #indekslenemez 
+
+# fruits.add('cherry')
+# fruits.update(['mango','grape','apple']) #varsa yeniden eklemez!
+# fruits.remove('mango')
+# fruits.discard('apple')
+# print(fruits)
+# fruits.pop() #normalde indeksli olduğu için son eleman/burada indekslenemez ilk eleman
+# print(fruits)
+# fruits.pop() #normalde indeksli olduğu için son eleman/burada indekslenemez ilk eleman
+# print(fruits)
+
+# myList = [1,5,7,9,4,6,4,5,5,4,6,1]
+# print(myList)
+# print(set(myList)) #aynı elemanları 1 defa yazar
+
+#############################################
+
+
+
+
+
+
